fb62edc184b3e6eabd7388d21932fa899ce88a9c
Steve Tom <tomsteve187@gmail.com>
2023-06-05 12:56:38 +0300

    Added style on login and reg

--- FrontEnd/wanunuzi/src/components/SignIn.jsx
+++ FrontEnd/wanunuzi/src/components/SignIn.jsx
@@ -18,23 +18,29 @@ const SignIn = () => {
     };
 
     return (
-        <form className="bg-amber-300 flex flex-col gap-2 m-2" onSubmit={handleSubmit}>
-            <h2 className="text-3xl font-bold underline">Sign In</h2>
-            <input className="input input-bordered w-full max-w-xs"
-                type="text"
-                placeholder="Enter Username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-            />
-            <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            <button type="submit">Sign In</button>
-            <p>{response}</p>
-        </form>
+        <div className="w-full flex flex-col place-items-center h-[90vh] justify-center">
+            <form className="w-80 flex flex-col gap-3 justify-center place-content-center place-items-center" onSubmit={handleSubmit}>
+                <h2 className="text-3xl font-bold text-green-900">Login</h2>
+                <input className="input input-bordered w-full max-w-xs"
+                       type="text"
+                       placeholder="Enter Username"
+                       required={true}
+                       value={username}
+                       onChange={(e) => setUsername(e.target.value)}
+                />
+                <input className="input input-bordered w-full max-w-xs"
+                       type="password"
+                       placeholder="Password"
+                       value={password}
+                       required={true}
+                       onChange={(e) => setPassword(e.target.value)}
+                />
+                <button className="btn w-full bg-green-400 ring-2 ring-red-600" type="submit">Sign In</button>
+                <p>{response}</p>
+                <p>Don't have an account? <a href="/register">Sign Up</a></p>
+            </form>
+        </div>
+
     );
 };
 
