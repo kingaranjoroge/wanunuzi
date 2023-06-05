fccb4bdb81bd0d4ccfe6446945d8ce158dcd3ddf
Steve Tom <tomsteve187@gmail.com>
2023-06-05 15:20:23 +0300

    Added style on login and reg

--- FrontEnd/wanunuzi/src/components/SignUp.jsx
+++ FrontEnd/wanunuzi/src/components/SignUp.jsx
@@ -18,33 +18,65 @@ const SignUp = () => {
 
     return (
         <div className="w-full flex flex-col place-items-center h-[90vh] justify-center">
-        <form className="w-80 flex flex-col gap-3 justify-center place-content-center place-items-center" onSubmit={handleSubmit}>
+        <form className="w-80 md:w-1/2 flex flex-col gap-3 justify-center place-content-center place-items-center" onSubmit={handleSubmit}>
             <h2 className="text-3xl font-bold text-green-900">Join Us</h2>
-            <input className="input input-bordered input-success w-full max-w-xs"
-                type="text"
-                   required={true}
-                placeholder="Username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-            />
-            <input className="input input-bordered input-success w-full max-w-xs "
-                type="password"
-                   required={true}
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            <input className="input input-bordered input-success w-full max-w-xs"
-                type="password"
-                     required={true}
-                placeholder="Confirm Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
+            <div className="flex md:flex-row flex-col gap-2 w-full">
+
+            <div className="flex w-full md:w-1/2  flex-col gap-2">
+
+                <input className="input input-bordered input-success w-full max-w-xs"
+                       type="text"
+                       required={true}
+                       placeholder="Username"
+                       value={username}
+                       onChange={(e) => setUsername(e.target.value)}
+                />
+                <input className="input input-bordered input-success w-full max-w-xs "
+                       type="password"
+                       required={true}
+                       placeholder="Password"
+                       value={password}
+                       onChange={(e) => setPassword(e.target.value)}
+                />
+                <input className="input input-bordered input-success w-full max-w-xs"
+                       type="password"
+                       required={true}
+                       placeholder="Confirm Password"
+                       value={password}
+                       onChange={(e) => setPassword(e.target.value)}
+                />
+            </div>
+            <div className="flex w-full md:w-1/2 flex-col gap-2">
+                <input className="input input-bordered input-success w-full max-w-xs"
+                       type="text"
+                       required={true}
+                       placeholder="Username"
+                       value={username}
+                       onChange={(e) => setUsername(e.target.value)}
+                />
+                <input className="input input-bordered input-success w-full max-w-xs "
+                       type="password"
+                       required={true}
+                       placeholder="Password"
+                       value={password}
+                       onChange={(e) => setPassword(e.target.value)}
+                />
+                <input className="input input-bordered input-success w-full max-w-xs"
+                       type="password"
+                       required={true}
+                       placeholder="Confirm Password"
+                       value={password}
+                       onChange={(e) => setPassword(e.target.value)}
+                />
+            </div>
+
+            </div>
             <button className="btn w-full bg-green-400 ring-red-500 ring-offset-2 ring-2" type="submit">Register</button>
             <p>{response}</p>
             <p>Already have an account? <a href="/login">Sign In</a></p>
         </form>
+
+
     </div>
     );
 };
