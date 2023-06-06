2ca1927eb88ee45321d4ef3cb490cc893c4f7e27
kingaranjoroge <briankingara4@gmail.com>
2023-06-06 09:46:22 +0300

    Added authentication using jwt

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('./models/User');
 const cors = require('cors'); // don't forget to install this: npm install cors
+const cookieParser = require('cookie-parser');
 require('dotenv').config(); // don't forget to install this: npm install dotenv
 
 const app = express();
@@ -12,51 +13,61 @@ app.use(express.json());
 
 app.post('/signup', async (req, res) => {
     try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        //verify that the email is not already in use
-        const emailExists = await User.findOne({ where: { email: req.body.email } });
-        if (emailExists) {
-            return res.status(500).json({ message: 'Email already in use' });
-        }
-        //verify that the phone number is not already in use
-        const phoneNumberExists = await User.findOne({ where: { phoneNumber: req.body.phoneNumber } });
-        if (phoneNumberExists) {
-            return res.status(500).json({ message: 'Phone number already in use' });
-        }
-        //verify that the id number is not already in use
-        const idNumberExists = await User.findOne({ where: { idNumber: req.body.idNumber } });
-        if (idNumberExists) {
-            return res.status(500).json({ message: 'ID number already in use' });
-        }
-
-        const user = await User.create({
-            fullName: req.body.fullName,
-            email: req.body.email,
-            phoneNumber: req.body.phoneNumber,
-            idNumber: req.body.idNumber,
-            password: hashedPassword
-        });
-        res.status(201).json({ message: 'User created successfully', username: user.username });
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+      // Verify that the email is not already in use
+      const emailExists = await User.findOne({ where: { email: req.body.email } });
+      if (emailExists) {
+        return res.status(500).json({ message: 'Email already in use' });
+      }
+      // Verify that the phone number is not already in use
+      const phoneNumberExists = await User.findOne({ where: { phoneNumber: req.body.phoneNumber } });
+      if (phoneNumberExists) {
+        return res.status(500).json({ message: 'Phone number already in use' });
+      }
+      // Verify that the ID number is not already in use
+      const idNumberExists = await User.findOne({ where: { idNumber: req.body.idNumber } });
+      if (idNumberExists) {
+        return res.status(500).json({ message: 'ID number already in use' });
+      }
+  
+      const user = await User.create({
+        fullName: req.body.fullName,
+        email: req.body.email,
+        phoneNumber: req.body.phoneNumber,
+        idNumber: req.body.idNumber,
+        password: hashedPassword
+      });
+  
+      // Generate a JWT token for the newly registered user
+      const token = jwt.sign({ username: user.username }, process.env.JWT_SECRET, {
+        expiresIn: '1h' // Token expiration time (optional)
+      });
+  
+      res.status(201).json({ message: 'User created successfully', token, username: user.username });
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Error creating user' });
+      console.error(error);
+      res.status(500).json({ message: 'Error creating user' });
     }
-});
+  });
+  
 
 app.post('/login', async (req, res) => {
     try {
-        const user = await User.findOne({ where: { email: req.body.username } });
-        if (user && await bcrypt.compare(req.body.password, user.password)) {
-            const token = jwt.sign({ username: user.username }, process.env.JWT_SECRET);
-            res.json({ message: 'Logged in successfully', token });
-        } else {
-            res.status(401).json({ message: 'Invalid username or password' });
-        }
+      const user = await User.findOne({ where: { email: req.body.username } });
+      if (user && await bcrypt.compare(req.body.password, user.password)) {
+        const token = jwt.sign({ username: user.username }, process.env.JWT_SECRET, {
+          expiresIn: '1h' // Token expiration time (optional)
+        });
+        res.json({ message: 'Logged in successfully', token });
+      } else {
+        res.status(401).json({ message: 'Invalid username or password' });
+      }
     } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Error logging in' });
+      console.error(error);
+      res.status(500).json({ message: 'Error logging in' });
     }
-});
+  });
+  
 
 const port = process.env.PORT || 3000;
 
--- FrontEnd/wanunuzi/src/components/SignIn.jsx
+++ FrontEnd/wanunuzi/src/components/SignIn.jsx
@@ -2,47 +2,52 @@ import React, { useState } from "react";
 import axios from 'axios';
 
 const SignIn = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [response, setResponse] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [response, setResponse] = useState("");
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        try {
-            const res = await axios.post('http://localhost:3000/login', { username, password });
-            setResponse(res.data.message);
-            window.location.href = "/home";
-        } catch (error) {
-            //console.error(error);
-            setResponse(error.response.data.message);
-        }
-    };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const res = await axios.post('http://localhost:3000/login', { username, password });
+      const { message, token } = res.data;
+      setResponse(message);
+      
+      // Store the token in local storage
+      localStorage.setItem('token', token);
+      console.log(token)
+      
+      // Redirect to the home page or perform other actions
+      window.location.href = "/home";
+    } catch (error) {
+      setResponse(error.response.data.message);
+    }
+  };
 
-    return (
-        <div className="w-full flex flex-col place-items-center h-[90vh] justify-center">
-            <form className="w-80 flex flex-col gap-3 justify-center place-content-center place-items-center" onSubmit={handleSubmit}>
-                <h2 className="text-3xl font-bold text-green-900">Login</h2>
-                <input className="input input-bordered w-full max-w-xs"
-                       type="text"
-                       placeholder="Enter Email"
-                       required={true}
-                       value={username}
-                       onChange={(e) => setUsername(e.target.value)}
-                />
-                <input className="input input-bordered w-full max-w-xs"
-                       type="password"
-                       placeholder="Password"
-                       value={password}
-                       required={true}
-                       onChange={(e) => setPassword(e.target.value)}
-                />
-                <button className="btn w-full bg-green-400 ring-2 ring-red-600" type="submit">Sign In</button>
-                <p>{response}</p>
-                <p>Don't have an account? <a href="/register">Sign Up</a></p>
-            </form>
-        </div>
-
-    );
+  return (
+    <div className="w-full flex flex-col place-items-center h-[90vh] justify-center">
+      <form className="w-80 flex flex-col gap-3 justify-center place-content-center place-items-center" onSubmit={handleSubmit}>
+        <h2 className="text-3xl font-bold text-green-900">Login</h2>
+        <input className="input input-bordered w-full max-w-xs"
+          type="text"
+          placeholder="Enter Email"
+          required={true}
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input className="input input-bordered w-full max-w-xs"
+          type="password"
+          placeholder="Password"
+          value={password}
+          required={true}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="btn w-full bg-green-400 ring-2 ring-red-600" type="submit">Sign In</button>
+        <p>{response}</p>
+        <p>Don't have an account? <a href="/register">Sign Up</a></p>
+      </form>
+    </div>
+  );
 };
 
 export default SignIn;
