6e539b4c0a9e29ecb76a07e29fcdfde987a511cf
Steve Tom <tomsteve187@gmail.com>
2023-06-07 13:14:57 +0300

    Fixed Right nav

--- FrontEnd/wanunuzi/src/components/SideBar.jsx
+++ FrontEnd/wanunuzi/src/components/SideBar.jsx
@@ -12,18 +12,6 @@ function NavRight() {
         navigate("/login");
     }
     return (
-<<<<<<< HEAD
-        <div className="navbar bg-green-700 justify-center w-[70px] h-full flex flex-col items-center text-black">
-            <i className="text-2xl my-8">
-                <i className="far fa-money-bill-transfer"></i>
-            </i>
-            <i className="text-2xl my-8">
-                <i className="fas fa-money-bill-transfer"></i>
-            </i>
-            <i className="text-2xl my-8">
-                <i className="fas fa-address-card"></i>
-            </i>
-=======
         <div className="navbar bg-green-700 w-[70px] h-full flex flex-col text-black">
             <a className="flex flex-col items-center my-3">
                 <i className="fa-solid btn btn-circle ring-green-800 text-2xl ring-2 ring-inset bg-green-700 fa-building-columns"></i>
@@ -37,7 +25,6 @@ function NavRight() {
                 <i className="fa-solid btn btn-circle ring-green-800  text-2xl ring-2 ring-inset bg-green-700 fa-circle-info"></i>
                 <p className="text-xs">About</p>
             </a>
->>>>>>> 1c2147fc3ea207f8fb18e7672caa66d37b186900
         </div>
     );
 }
