2032ebb1119d169c647695bc18fb1ce67ef05da2
kingaranjoroge <briankingara4@gmail.com>
2023-06-07 11:34:23 +0300

    Added sidebar icons

--- FrontEnd/wanunuzi/src/components/SideBar.jsx
+++ FrontEnd/wanunuzi/src/components/SideBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link, useNavigate} from 'react-router-dom';
+
 function NavRight() {
 
     const navigate = useNavigate();
@@ -12,8 +13,19 @@ function NavRight() {
         navigate("/login");
     }
     return (
-        <div className="navbar bg-green-700 justify-center w-[100px] h-full">
+        <div className="navbar bg-green-700 justify-center w-[100px] h-full flex flex-col items-center text-black">
+            <i className="text-2xl my-8">
+                <i className="far fa-money-bill-transfer"></i>
+            </i>
+            <i className="text-2xl my-8">
+                <i className="fas fa-money-bill-transfer"></i>
+            </i>
+            <i className="text-2xl my-8">
+                <i className="fas fa-address-card"></i>
+            </i>
         </div>
+
+
     );
 }
 
