318c06865edfb3578b7aee80e85f8682cf0e9d53
kingaranjoroge <briankingara4@gmail.com>
2023-06-08 22:25:29 +0300

    Revert "Made path register index path"
    This reverts commit 5178d1f0393502c8987ebd65f07f928cb78c5eca.

--- FrontEnd/wanunuzi/src/components/App.jsx
+++ FrontEnd/wanunuzi/src/components/App.jsx
@@ -3,9 +3,15 @@ import Layout from './Layout.jsx';
 import SignIn from './SignIn.jsx';
 import SignUp from './SignUp.jsx';
 import Homepage from './Homepage.jsx';
+<<<<<<< HEAD
 import Payment from './Payment.jsx';
+import NavBar from './NavBar.jsx';
+import NavBeforeLogin from './NavBarUnlogged.jsx';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import 'tailwindcss/tailwind.css';
+=======
+import jwtDecode from 'jwt-decode';
+>>>>>>> ba6c0eacb6eb40faeb697770286b849fac4382cb
 
 const ProtectedRoute = ({children}) => {
     const token = localStorage.getItem('token');
@@ -21,11 +27,18 @@ const ProtectedRoute = ({children}) => {
 
 function App() {
     return (
+<<<<<<< HEAD
         <Routes>
             <Route path="/" element={<Layout />} >
-                <Route index element={<SignUp />} />                    
+                <Route path="/register" element={<SignUp />} />                    
                 <Route path="/payment" element={<Payment />} />
                 <Route path="/login" element={<SignIn />} />            
+=======
+        <Router>
+            <Routes>
+                <Route path="/login" element={<SignIn />} />
+                <Route path="/register" element={<SignUp />} />
+>>>>>>> ba6c0eacb6eb40faeb697770286b849fac4382cb
                 <Route path="/home" element={<ProtectedRoute><Homepage /></ProtectedRoute>} />
                 <Route path="*" element={<Navigate to="/login" />} />
                 <Route path="loan" element={<ProtectedRoute><Loan /></ProtectedRoute>} />
