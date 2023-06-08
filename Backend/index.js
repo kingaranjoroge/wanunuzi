5178d1f0393502c8987ebd65f07f928cb78c5eca
kingaranjoroge <briankingara4@gmail.com>
2023-06-08 20:57:00 +0300

    Made path register index path

--- Backend/index.js
+++ Backend/index.js
@@ -50,27 +50,23 @@ app.post('/signup', async (req, res) => {
       res.status(500).json({ message: 'Error creating user' });
     }
   });
-
+  
 
 app.post('/login', async (req, res) => {
-  try {
-    const user = await User.findOne({ where: { email: req.body.username } });
-    if (user && await bcrypt.compare(req.body.password, user.password)) {
-      const token = jwt.sign({
-        name: user.fullName,  // Corrected field reference
-        email: user.email,
-        idNumber: user.idNumber,  // Added field reference
-        phoneNumber: user.phoneNumber,  // Added field reference
-        userId: user.id
-        // Add other user details you might want to include
-      }, process.env.JWT_SECRET, {
-        expiresIn: '5m' // Token expiration time (optional)
-      });
-      res.json({ message: 'Logged in successfully', token });
-    } else {
-      res.status(401).json({ message: 'Invalid username or password' });
+    try {
+      const user = await User.findOne({ where: { email: req.body.username } });
+      if (user && await bcrypt.compare(req.body.password, user.password)) {
+        const token = jwt.sign({ username: user.username }, process.env.JWT_SECRET, {
+          expiresIn: '1h' // Token expiration time (optional)
+        });
+        res.json({ message: 'Logged in successfully', token });
+      } else {
+        res.status(401).json({ message: 'Invalid username or password' });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Error logging in' });
     }
-<<<<<<< HEAD
   });
 
 
@@ -100,14 +96,6 @@ app.post('/payment', async (req, res) => {
     res.status(500).json({ success: false, message: 'Failed to initiate payment' });
   }
 });
-=======
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Error logging in' });
-  }
-});
-
->>>>>>> ba6c0eacb6eb40faeb697770286b849fac4382cb
   
 
 const port = process.env.PORT || 3000;
