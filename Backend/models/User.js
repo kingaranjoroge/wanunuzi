5cb0f33a0aa3525ba5fb515e1c62a4465c5eb1e5
Steve Tom <tomsteve187@gmail.com>
2023-06-09 12:09:15 +0300

    Unversioned files

---
+++ Backend/models/User.js
@@ -0,0 +1,43 @@
+const { Sequelize, DataTypes, Model } = require('sequelize');
+const sequelize = new Sequelize('wanunuzi_db', 'root', '', {dialect: 'mariadb'});
+
+class User extends Model {}
+
+User.init({
+    fullName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    idNumber: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    phoneNumber: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    emailVerificationToken: {
+        type: DataTypes.STRING,
+    },
+    emailVerified: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+    },
+}, {
+    sequelize,
+    modelName: 'User'
+});
+
+module.exports = User;
