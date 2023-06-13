0245f5624a822526c3e4a3c563735895fa334cbe
kingaranjoroge <briankingara4@gmail.com>
2023-06-13 11:39:20 +0300

    Added Migrations

---
+++ Backend/models/Guarantor.js
@@ -0,0 +1,51 @@
+const { Sequelize, DataTypes, Model } = require('sequelize');
+const sequelize = new Sequelize('wanunuzi_db', 'root', '', {dialect: 'mariadb'});
+
+class Guarantor extends Model {}
+
+Guarantor.init({
+    loanId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'loans',
+          key: 'id'
+        }
+      },
+      guarantor1: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'users',
+            key: 'id'
+        }
+      },
+      guarantor2: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'users',
+            key: 'id'
+        }
+      },
+      guarantor3: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'users',
+            key: 'id'
+        }
+      }
+    }, {
+        sequelize,
+        modelName: 'Guarantor'
+    });
+Guarantor.associateModels = () => {
+    const Loan = require('./Loan');
+    const User = require('./User')
+
+    Guarantor.belongsTo(Loan, { foreignKey: 'loanId' });
+    Guarantor.belongsTo(User, { foreignKey: 'userId' });
+}
+
+module.exports = Guarantor
