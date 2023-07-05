049c5f1efffbfb8c981d5e02a110b2dc31737b67
kingaranjoroge <briankingara4@gmail.com>
2023-07-05 15:27:36 +0300

    added admin manage users logic

--- Backend/routes/usersRoute.js
+++ Backend/routes/usersRoute.js
@@ -1,8 +1,12 @@
-const express = require('express')
-const router = express.Router()
-const usersController = require('../controllers/usersController')
+const express = require('express');
+const router = express.Router();
+const usersController = require('../controllers/usersController');
 
-router.route('/')
-    .post(usersController.createUser)
+router.route('/').post(usersController.createUser);
+router.route('/:id').get(usersController.getUserById);
+router.route('/').get(usersController.getAllUsers);
+router.route('/:id').put(usersController.updateUser);
+router.route('/:id').patch(usersController.updateUser);
+router.route('/:id').delete(usersController.deleteUser);
 
-module.exports = router
+module.exports = router;
