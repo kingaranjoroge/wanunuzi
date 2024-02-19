53446dc967f392f9431c40b881ca03e6ce35ee64
kingaranjoroge <111523863+kingaranjoroge@users.noreply.github.com>
2024-02-19 12:15:21 +0300

    modified files

--- Backend/routes/usersRoute.js
+++ Backend/routes/usersRoute.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const router = express.Router();
-const usersController = require('../controllers/usersController');
-
-router.route('/').post(usersController.createUser);
-router.route('/:id').get(usersController.getUserById);
-router.route('/').get(usersController.getAllUsers);
-router.route('/:id').put(usersController.updateUser);
-router.route('/:id').patch(usersController.updateUser);
-router.route('/:id').delete(usersController.deleteUser);
-router.route('/:id/paymentStatus').get(usersController.getPaymentStatus);
-
-module.exports = router;
-=======
 const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/usersController');
@@ -28,4 +13,4 @@ router.route('/:id').patch(usersController.updateUser);
 router.route('/:id').delete(usersController.deleteUser);
 
 module.exports = router;
->>>>>>> 73032f202ab021e03e2bc60d6370cf99c3a84f4e
+
